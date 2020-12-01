b335f6749c1f5122fe7c26671416e774087171b6
卢松林 <2445859460@qq.com>
2020-12-01 18:27:43 +0800

    add open-feign服务调用demo

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @project: cloud2020
+ * @description:
+ * @author: SongLin.Lu
+ * @date: 2020/12/1 - 18:19
+ */
+@RestController
+@Slf4j
+public class OrderFeignController {
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/comsumer/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+        return paymentFeignService.getPaymentById(id);
+    }
+}
