8fdd5f0d691dad9544beb3bbbfb46310d085e23c
卢松林 <2445859460@qq.com>
2020-12-01 18:46:24 +0800

    add open-feign超时控制demo

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @project: cloud2020
@@ -63,4 +64,11 @@ public class PaymentController {
         return serverPort;
     }
 
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout()
+    {
+        // 业务逻辑处理正确，但是需要耗费3秒钟
+        try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
+        return serverPort;
+    }
 }
