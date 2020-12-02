939c52d183f8c8b332b83ddd38bcb587e73eec66
卢松林 <2445859460@qq.com>
2020-12-02 11:14:15 +0800

    add cloud-consumer-feign-hystrix-order80 add 微服务调用支付服务

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @project: cloud2020
+ * @description:
+ * @author: SongLin.Lu
+ * @date: 2020/12/2 - 10:57
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+}
