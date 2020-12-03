fad5de6644b502f7066b787ef3c206583cb4dce2
卢松林 <2445859460@qq.com>
2020-12-03 10:16:47 +0800

    add GateWay路由配置的两种方式

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @project: cloud2020
+ * @description: 网关 配置类
+ * @author: SongLin.Lu
+ * @date: 2020/12/3 - 10:06
+ */
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_rote_atguigu", r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator2(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_rote_atguigu2", r -> r.path("/guoji").uri("http://news.baidu.com/guonji")).build();
+        return routes.build();
+    }
+}
