ad058569c86575781b5008504b7a03ea44929831
卢松林 <2445859460@qq.com>
2020-12-03 14:15:44 +0800

    add 配置中心客户端并测试

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @project: cloud2020
+ * @description:
+ * @author: SongLin.Lu
+ * @date: 2020/12/3 - 14:04
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
