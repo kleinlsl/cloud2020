cff90f3438b32600303f498364ffc42183f77295
卢松林 <2445859460@qq.com>
2020-12-03 10:59:20 +0800

    add GateWay常用的Predicate(断言)

---
+++ cloud-gateway-gateway9527/src/test/java/Test.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @project: cloud2020
+ * @description:
+ * @author: SongLin.Lu
+ * @date: 2020/12/3 - 10:33
+ */
+public class Test {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime=ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
