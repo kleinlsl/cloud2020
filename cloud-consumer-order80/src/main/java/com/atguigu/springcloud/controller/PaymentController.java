1b1420c8195b94106e724f13bc9576d377e10766
卢松林 <2445859460@qq.com>
2020-12-08 11:15:21 +0800

    update cloud-consumer-order80 update cloud-provider-payment8001 SpringCloud Sleuth分布式请求链路跟踪

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -73,4 +73,11 @@ public class PaymentController {
         return restTemplate.getForObject(uri+"/payment/lb",String.class);
     }
 
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin()
+    {
+        String result = restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/", String.class);
+        return result;
+    }
+
 }
