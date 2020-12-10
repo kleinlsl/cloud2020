a7a77c2ff6b487ba77263bd21b1957be8e3f57b7
卢松林 <2445859460@qq.com>
2020-12-10 11:35:23 +0800

    add Sentinel服务熔断功能之Feign系列

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.stereotype.Component;
+
+/**
+ * @project: cloud2020
+ * @description:
+ * @author: SongLin.Lu
+ * @date: 2020/12/10 - 11:25
+ */
+@Component
+public class PaymentFallbackService implements PaymentService
+{
+    @Override
+    public CommonResult<Payment> paymentSQL(Long id)
+    {
+        return new CommonResult<>(44444,"服务降级返回,---PaymentFallbackService",new Payment(id,"errorSerial"));
+    }
+}
