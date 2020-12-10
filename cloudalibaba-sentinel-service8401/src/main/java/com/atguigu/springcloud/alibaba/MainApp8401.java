a4c61c39325b3b2a2b7fcacf71806fd554a170ac
卢松林 <2445859460@qq.com>
2020-12-10 09:51:50 +0800

    add Sentinel 1、流控规则 2、降级规则 3、热点key规则 4、系统规则

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/MainApp8401.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @project: cloud2020
+ * @description:
+ * @author: SongLin.Lu
+ * @date: 2020/12/9 - 14:27
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class,args);
+    }
+}
